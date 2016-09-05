b77b01f99164d1be87b26788dcfa82734e16ae38
okieoth <eiko.thomas@gmx.net>
2016-09-05 10:31:10 +0200

    cless and stuff

--- standard_stuff.sh
+++ standard_stuff.sh
@@ -3,4 +3,17 @@ function checkDiskWrite() {
     awk '/sd/ {print $3"\t"$10 / 2 / 1024}' /proc/diskstats
 }
 
+clessFunc() {
+	pygmentize -g $1 | less -R
+}
+
+clessFunc_lnr() {
+	pygmentize -g -O style=colorful,linenos=1 $1 | less -R
+}
+
 alias g='grep'
+alias ccat='pygmentize -g'
+alias ccat_lnr='pygmentize -g -O style=colorful,linenos=1a'
+alias cless=clessFunc
+alias cless_lnr=clessFunc_lnr
+
