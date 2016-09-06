2af143d80ba98177e6bf6f51f303f12835b45d5f
okieoth <eiko.thomas@gmx.net>
2016-09-06 15:47:45 +0200

    docker_stuff fixed error in regexp for detect container names

--- docker_stuff.sh
+++ docker_stuff.sh
@@ -1,8 +1,8 @@
 function docker_info_base() {
 
 
-docker ps | sed -r 's-^[a-z0-9]*\W*([a-z0-9\.:]*)\W.*\W([a-z0-9_]*)$-C_R: \2 \-> \1-' | tail -n +2 | sed -e 's-#-\\-g'| awk '{print "\033[1;32m"$0"\033[0m"}';\
-docker ps -a | grep Exited | sed -r 's-^[a-z0-9]*\W*([a-z0-9\.:]*)\W.*\W([a-z0-9_]*)$-C_H: \2 \-> \1-' | sed -e 's-#-\\-g'| awk '{print "\033[1;36m"$0"\033[0m"}';\
+docker ps | sed -r 's-^[a-z0-9]*\W*([a-z0-9\.:_-]*)\W.*\s([a-z0-9_-]*)$-C_R: \2 \-> \1-' | tail -n +2 | sed -e 's-#-\\-g'| awk '{print "\033[1;32m"$0"\033[0m"}';\
+docker ps -a | grep Exited | sed -r 's-^[a-z0-9]*\W*([a-z0-9\.:_-]*)\W.*\s([a-z0-9_-]*)$-C_H: \2 \-> \1-' | sed -e 's-#-\\-g'| awk '{print "\033[1;36m"$0"\033[0m"}';\
 docker images | awk '{print "\033[1;34mIMG: "$1":"$2" \033[0m"}' | tail -n +2
 }
 
