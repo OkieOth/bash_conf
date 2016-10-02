f0f7e490ebed4a19fd96143f1a627ace1788d8cb
okieoth <eiko.thomas@gmx.net>
2016-10-02 22:54:40 +0200

    repair error in di alias

--- docker_stuff.sh
+++ docker_stuff.sh
@@ -1,9 +1,7 @@
 function docker_info_base() {
-
-
-docker ps | sed -r 's-^[a-z0-9]*\W*([a-z0-9\.:_-]*)\W.*\s([a-z0-9_-]*)$-C_R: \2 \-> \1-' | tail -n +2 | sed -e 's-#-\\-g'| awk '{print "\033[1;32m"$0"\033[0m"}';\
-docker ps -a | grep Exited | sed -r 's-^[a-z0-9]*\W*([a-z0-9\.:_-]*)\W.*\s([a-z0-9_-]*)$-C_H: \2 \-> \1-' | sed -e 's-#-\\-g'| awk '{print "\033[1;36m"$0"\033[0m"}';\
-docker images | awk '{print "\033[1;34mIMG: "$1":"$2" \033[0m"}' | tail -n +2
+    docker ps | sed -r 's-^[a-z0-9]*\W*([a-zA-Z0-9\.:_\/-]*)\W.*\s([a-zA-Z0-9\._-]*)$-C_R: \2 \-> \1-' | tail -n +2 | sed -e 's-#-\\-g'| awk '{print "\033[1;32m"$0"\033[0m"}';\
+    docker ps -a | grep Exited | sed -r 's-^[a-zA-Z0-9]*\W*([a-zA-Z0-9\.:_\/-]*)\W.*\s([a-zA-Z0-9\._-]*)$-C_H: \2 \-> \1-' | sed -e 's-#-\\-g'| awk '{print "\033[1;36m"$0"\033[0m"}';\
+    docker images | awk '{print "\033[1;34mIMG: "$1":"$2" \033[0m"}' | tail -n +2
 }
 
 function docker_info() {
