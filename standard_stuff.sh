466e3219748295e53ca6f10ed8df9b410b63a865
okieoth <eiko.thomas@gmx.net>
2016-10-02 22:54:06 +0200

    some aliases for grep, awk and xargs

--- standard_stuff.sh
+++ standard_stuff.sh
@@ -11,9 +11,14 @@ clessFunc_lnr() {
 	pygmentize -g -O style=colorful,linenos=1 $1 | less -R
 }
 
+a() {
+    awk "{print \$$1}"
+}
+
 alias g='grep'
 alias ccat='pygmentize -g'
 alias ccat_lnr='pygmentize -g -O style=colorful,linenos=1a'
 alias cless=clessFunc
 alias cless_lnr=clessFunc_lnr
+alias x='xargs'
 
